1c255e1c6609fd69446e75bb4da8a468e8982b89
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-09-22 11:49:45 +0200

    dev: added small circles

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ console.log("start");
 const samples = 10;
 const r = 1;
 
+const x0 = 50;
+const y0 = 50;
+const r0 = 45;
+
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("g.samples");
 for (let i = 0; i < samples; i++) {
-  const x = 47 + i * 5;
-  const y = 32;
+  const angle = (i * 2 * Math.PI) / samples;
+  const x = x0 + r0 * Math.cos(angle);
+  const y = y0 + r0 * Math.sin(angle);
 
   const circle = document.createElementNS(svgns, "circle");
   circle.setAttributeNS(null, "cx", x);
