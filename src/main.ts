df02a29e874eab702c678827833c179b42c31d9a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-09-22 14:50:20 +0200

    dev: typescript works.

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,17 @@ const r0 = 45;
 
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("g.samples");
+if (container === null) {
+  throw new Error("container not found.");
+}
 for (let i = 0; i < samples; i++) {
   const angle = (i * 2 * Math.PI) / samples;
   const x = x0 + r0 * Math.cos(angle);
   const y = y0 + r0 * Math.sin(angle);
 
   const circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", x);
-  circle.setAttributeNS(null, "cy", y);
-  circle.setAttributeNS(null, "r", r);
+  circle.setAttributeNS(null, "cx", x + "");
+  circle.setAttributeNS(null, "cy", String(y));
+  circle.setAttributeNS(null, "r", r.toString());
   container.appendChild(circle);
 }
